c186eaf1b2be8c554d49d2de709f0db54774bbc9
takyorama <takyorama@gmail.com>
2019-09-23 19:47:12 -0300

    dessafio backend

---
+++ cadastro.blade.php
@@ -0,0 +1,18 @@
+@extends('contatos')
+
+@section('conteudo')
+  <h2>Cadastro de contato</h2>
+  <div id="" class="">
+    <label for="">Nome: <input type="" name="" class=""/></label>
+  </div>
+  
+  <div id="" class="">
+    <label for="">EMAIL: <input type="" name="" class=""/></label>
+  </div>
+  <div id="" class="">
+   Telefone:
+   <input="" type="" name="" class=""/>
+   <input="" type="" name="" class=""/>
+  </div>
+  <button type="submit">Cadastrar</button>
+@endsection
